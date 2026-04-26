0aec7c681bb29169c7e9a75ccc0dcd68e5b39383
00MOREIRA00 <robertocorreamoreiraneto@gmail.com>
2026-04-26 09:40:43 -0300

    feat: adicionando configuracoes iniciais

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Bem-vindo ao Sistema de Gerenciamento de Ninjas!");
+        int execucao = 1;
+
+        while(execucao == 1) {
+            System.out.println("Selecione uma opção:");
+            System.out.println("1. Exibir informações de todos os ninjas");
+            System.out.println("2. Adicionar um novo ninja");
+            System.out.println("3. Remover um ninja");
+            System.out.println("4. Atualizar habilidades especiais de um ninja");
+            System.out.println("5. Sair");
+        }
+
+    }
+}
+
+
+// Menu Interativo
+// Exisbir informações de todos os ninjas
+// Adicionar novos ninjas
+// Remover Ninjas
+// Atualizar habilidades Especiais
