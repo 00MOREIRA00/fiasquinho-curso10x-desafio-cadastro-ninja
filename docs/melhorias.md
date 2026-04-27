cbbbfdaaef600f7bee2783c948e814162b050d71
00MOREIRA00 <robertocorreamoreiraneto@gmail.com>
2026-04-27 09:52:13 -0300

    feat: adicionando funcionalidade inicial de cadastro de busca, e adicao de docs

---
+++ docs/melhorias.md
@@ -0,0 +1,11 @@
+# Melhorias 
+
+Esse documento tem como objetivo listar as melhorias que podem ser feitas no projeto, seja para corrigir bugs, otimizar o código ou adicionar novas funcionalidades.
+
+## Correção de Bugs
+- [ ] Corrigir o problema de cadastro do mesmo usuário mais de uma vez, garantindo que o sistema não permita duplicatas.
+
+## Melhoriias 
+- [ ] Adicionar testes unitários para garantir a qualidade do código.
+- [ ] Refatorar o código para melhorar a legibilidade e manutenção.
+- [ ] Melhorar espaçamento entre impressão dos elementos para facilitar melhor visualização. 
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,10 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
+        ArrayList<String> ninjas = new ArrayList<>();
 
         System.out.println("Bem-vindo ao Sistema de Gerenciamento de Ninjas!");
         int execucao = 1;
@@ -20,10 +23,22 @@ public class Main {
             switch (opcao) {
                 case 1:
                     System.out.println("Você selecionou a opção 1: Exibir informações de todos os ninjas");
+                    if (ninjas.isEmpty()){
+                        System.out.println("Nenhum ninja cadastrado !!");
+                    }
+
+                    for (int i = 0; i < ninjas.size(); i++) {
+                        System.out.println("Ninja " + (i + 1) + ": " + ninjas.get(i));
+                    }
+
                     break;
 
                 case 2:
                     System.out.println("Você selecionou a opção 2: Adicionar um novo ninja");
+                    System.out.println("Digite o nome do ninja:");
+                    sc.nextLine();
+                    String nomeNinja = sc.nextLine();
+                    ninjas.add(nomeNinja);
                     break;
 
                 case 3:
@@ -48,8 +63,8 @@ public class Main {
 }
 
 
-// Menu Interativo                                  [OK]
-// Exisbir informações de todos os ninjas
-// Adicionar novos ninjas
-// Remover Ninjas
-// Atualizar habilidades Especiais
+// [OK] - Menu Interativo
+// [] - Exisbir informações de todos os ninjas
+// [] - Adicionar novos ninjas
+// [] - Remover Ninjas
+// [] - Atualizar habilidades Especiais
