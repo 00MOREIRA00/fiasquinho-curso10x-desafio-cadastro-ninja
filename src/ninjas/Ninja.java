dcfe9803552e707d12462ba04d73217a7733021a
00MOREIRA00 <robertocorreamoreiraneto@gmail.com>
2026-05-05 00:33:17 -0300

    feat: refatorando arquivo de Ninjas

--- src/ninjas/Ninja.java
+++ src/ninjas/Ninja.java
@@ -1,6 +1,7 @@
 package ninjas;
 
 public class Ninja {
+
     private String nome;
     private int idade;
     private String missao;
@@ -15,21 +16,29 @@ public class Ninja {
         this.statusMissao = statusMissao;
     }
 
+    public String getTipoCla() {
+        return "Comum";
+    }
+
+    public String getHabilidadeEspecial() {
+        return "Nenhuma";
+    }
+
+    public void setHabilidadeEspecial(String habilidade) {
+        // Ninja comum não possui habilidade especial
+    }
+
     public void mostrarInformacoes() {
         System.out.println("Nome: " + nome);
         System.out.println("Idade: " + idade);
         System.out.println("Missão: " + missao);
         System.out.println("Nível de Dificuldade: " + nivelDificuldade);
         System.out.println("Status da Missão: " + statusMissao);
+        System.out.println("Clã: " + getTipoCla());
+        System.out.println("Habilidade Especial: " + getHabilidadeEspecial());
     }
 
-    public String getNome() { return nome; }
-
-    public int getIdade() { return idade; }
-
-    public String getMissao() { return missao; }
-
-    public String getNivelDificuldade() { return nivelDificuldade; }
-
-    public String getStatusMissao() { return statusMissao; }
+    public String getNome() {
+        return nome;
+    }
 }
